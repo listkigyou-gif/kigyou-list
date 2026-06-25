3bbdcbdfc3093763e30a6e58442f000c7a7fd3f3
trung kim <email_cua_ban@example.com>
2026-06-26 00:19:29 +0700

    chore: skip type checking during build to save memory

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
